5e29f2d24448bd254f1b1c5c1c2468f7917ed6ff
infooo3 <119267685+infooo3@users.noreply.github.com>
2023-01-07 17:20:43 +0100

    Add files via upload

---
+++ back/middleware/auth.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    const userId = decodedToken.userId;
+    const isAdmin = decodedToken.isAdmin;
+    req.auth = {isAdmin, userId}
+    if (req.body.userId && req.body.userId !== userId) {
+      throw 'User ID non valable !';
+    } else { 
+      next();
+    }
+  } catch (error) {
+    res.status(401).json({ error: error | 'Requête non authentifiée !' });
+  }
+};
